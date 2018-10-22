7728d0367338f7709c2806b345409536e9b38ac8
Daniel Estevez <daniel.ef@gmail.com>
2018-10-22 16:43:24 -0500

    Standarized messages

--- src/clj/mx/interware/caudal/io/rest_server.clj
+++ src/clj/mx/interware/caudal/io/rest_server.clj
@@ -12,6 +12,7 @@
             [bidi.ring :as bidi]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.file :refer [wrap-file]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.session :refer [wrap-session]]
             [ring.middleware.cors :refer [wrap-cors]]
@@ -19,12 +20,14 @@
             [ring.middleware.gzip :refer [wrap-gzip]]
             [aleph.http :as aleph-http]
             [mx.interware.caudal.streams.common :refer [start-listener deflistener* comp-sink]]
-            [mx.interware.caudal.web.rest-handle :as handle])
+            [mx.interware.caudal.web.rest-handle :as handle]
+            [mx.interware.caudal.core.starter-dsl :refer [name&version]])
   (:import
     (io.netty.handler.ssl SslContextBuilder)))
 
-(def Dname "caudal")
-(def version "0.7.14")
+(def N&V (name&version))
+(def Dname (last (clojure.string/split (str (first N&V)) #"/")))
+(def version (second N&V))
 
 (defn not-found [request]
   {:status 404 :body (str (:uri request) " not found!")})
@@ -34,13 +37,13 @@
 (defn redirect2project [request]
      {:status 301 :headers {"location" (str "/" Dname)}})
 
-(def _ah-cmds (atom {}))
+#_(def _ah-cmds (atom {}))
 
-(defn register_ah-fn [cmd f]
+#_(defn register_ah-fn [cmd f]
   (log/info "Register fn for: " (pr-str cmd))
   (swap! _ah-cmds assoc cmd f))
 
-(register_ah-fn "hello" (fn [] {:status 200 :body (str "Hello World @ " (java.util.Date.))}))
+#_(register_ah-fn "hello" (fn [] {:status 200 :body (str "Hello World @ " (java.util.Date.))}))
 
 (defn index [routes]
   (let [nss (reduce (fn [result [path _]]
@@ -50,7 +53,8 @@
     (fn [request]
       (if-let [cmd-fn (->>
                        (get-in request [:route-params :cmd])
-                       (get @_ah-cmds))]
+                       ;(get @_ah-cmds)
+                       )]
         (cmd-fn)
         {:status 200
          :body {:name Dname
@@ -86,6 +90,7 @@
 (defn create-routes [sink]
   [[["/"] {{:request-method :get} handle/index-handler}]
    [["app"] {{:request-method :get} handle/index-handler}]
+   [["event"] {{:request-method :post} (partial handle/event-handler sink)}]
    [["event"] {{:request-method :put} (partial handle/event-handler sink)}]])
 
 (defn create-handler [publisher-handler sink states]
@@ -102,11 +107,14 @@
             publisher (wrap-cors cors)
             publisher wrap-session
             publisher (wrap-file "resources/public/" {:index-files? true})
+            publisher (wrap-content-type {:mime-types {nil "text/html"}})
             gzip (wrap-gzip))))
 
 (defn start-server [app {:keys [http-port https-port server-key server-key-pass server-crt] :as config}]
-  ;(println "\n\n\nStarting http: " (pr-str config))
-  (log/info "Starting http server " (if http-port (str "http@" http-port) " ") (if https-port (str "https@" https-port) ""))
+  (if http-port
+    (log/info "Starting HTTP Server, port:" http-port))
+  (if https-port
+    (log/info "Starting HTTPS Server, port:" https-port))
   (let [ssl-context-builder (and https-port
                                  server-key
                                  server-crt
@@ -118,16 +126,14 @@
                                                                 (file server-key))))
         https-serv (and ssl-context-builder (aleph-http/start-server app {:port https-port :ssl-context (.build ssl-context-builder)}))
         http-serv (and http-port (aleph-http/start-server app {:port http-port}))]
-    (if https-serv
-      (log/info "https server started on port: " https-port)
-      (and https-port (log/error "Can't start https server on port: " https-port)))
-    (if http-serv
-      (log/info "http server started on port: " http-port)
-      (and http-port (log/error "Can't start http server on port: " http-port)))
-    (log/debug {:rest-server config})
-    ))
+    (if-not http-serv
+      (and http-port (log/error "Can't start HTTP Server, port: " http-port)))
+    (if-not https-serv
+      (and https-port (log/error "Can't start HTTPS Server, port: " https-port)))
+    (log/debug {:rest-server config})))
 
 (defn start-rest-listener [sink config]
+  (log/debug {:sink sink :config config})
   (let [cors   (get-in config [:parameters :cors])
         gzip   (get-in config [:parameters :gzip])
         publisher (get-in config [:parameters :publisher])
