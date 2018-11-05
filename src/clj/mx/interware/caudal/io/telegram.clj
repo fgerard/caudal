d15faeb63e2abbb51b85b78ea1e8b2603d251215
Daniel Estevez <daniel.ef@gmail.com>
2018-11-05 04:28:50 -0600

    Replacing aleph with clj-http in telegram to fix some requests codes in 40X

--- src/clj/mx/interware/caudal/io/telegram.clj
+++ src/clj/mx/interware/caudal/io/telegram.clj
@@ -3,116 +3,117 @@
             [clojure.tools.logging :as log]
             [clojure.string :as string] 
             [clojure.data.json :as json]
-            [aleph.http :as http]
-            [byte-streams :as bs]
+            ;[aleph.http :as http]
+            ;[byte-streams :as bs]
+            [clj-http.client :as http]
             [mx.interware.caudal.util.ns-util :refer [resolve&get-fn]]
             [mx.interware.caudal.streams.common :as common :refer [propagate start-listener]]))
 
 (def base-url "https://api.telegram.org/bot")
+(http/with-connection-pool {:timeout 30 :threads 4 :insecure? false :default-per-route 4}
 
-(defn send-text*
-  "Sends message to the chat"
-  ([token chat-id text] (send-text* token chat-id {} text))
-  ([token chat-id options text]
-   (try
-     (let [url  (str base-url token "/sendMessage")
-           body (into {:chat_id chat-id :text text} options)
-           resp @(http/request {:request-method "post"
-                                :url url
-                                :headers {"Content-Type" "application/json"}
-                                :query-params body})]
-       (-> resp :body))
-     (catch clojure.lang.ExceptionInfo e
-       (log/error (-> e
-                      .getData
-                      :body
-                      bs/to-string))))))
+  (defn send-text*
+    "Sends message to the chat"
+    ([token chat-id text] (send-text* token chat-id {} text))
+    ([token chat-id options text]
+     (try
+       (let [url  (str base-url token "/sendMessage")
+             base-form    [{:name "chat_id" :content (str chat-id)}
+                           {:name "text" :content text}]
+             options-form (for [[key value] options]
+                            {:name (name key) :content (if (coll? value) (json/write-str value) value)})
+             form         (into base-form options-form)
+             resp         (http/post url {:multipart form})]
+         (-> resp :body))
+       (catch clojure.lang.ExceptionInfo e
+         (log/error (-> e
+                        .getData
+                        :body
+                        (json/read-str :key-fn keyword)
+                        pr-str))))))
 
-(defn send-file* [token chat-id options file method field filename]
-  "Helper function to send various kinds of files as multipart-encoded"
-  (try
-    (let [url          (str base-url token method)
-          base-form    [{:part-name "chat_id" :content (str chat-id)}
-                        {:part-name field :content file :name filename}]
-          options-form (for [[key value] options]
-                         {:part-name (name key) :content value})
-          form         (into base-form options-form)
-          resp         @(http/request {:request-method "post"
-                                       :url url
-                                       :headers {"Content-Type" "application/json"}
-                                       :multipart form})]
-      (-> resp :body))
-    (catch clojure.lang.ExceptionInfo e
-       (log/error (-> e
-                      .getData
-                      :body
-                      bs/to-string)))))
+  (defn send-file* [token chat-id options file method field filename]
+    "Helper function to send various kinds of files as multipart-encoded"
+    (try
+      (let [url          (str base-url token method)
+            base-form    [{:part-name "chat_id" :content (str chat-id)}
+                          {:part-name field :content file :name filename}]
+            options-form (for [[key value] options]
+                           {:part-name (name key) :content value})
+            form         (into base-form options-form)
+            resp         (http/post url {:multipart form})]
+        (-> resp :body))
+      (catch clojure.lang.ExceptionInfo e
+        (log/error (-> e
+                       .getData
+                       :body
+                       (json/read-str :key-fn keyword)
+                       pr-str)))))
 
-(defn send-text
-  [[key-token key-chat-id text options] & children]
-  (fn [by-path state event]
-    (let [token (key-token event)
-          chat-id (key-chat-id event)
-          text (if (or (fn? text) (keyword? text)) (text event) text)
-          options (or options {})]
-      (send-text* token chat-id options text))
-    (common/propagate by-path state event children)))
+  (defn send-text
+    [[key-token key-chat-id text options] & children]
+    (fn [by-path state event]
+      (let [token (key-token event)
+            chat-id (key-chat-id event)
+            text (if (or (fn? text) (keyword? text)) (text event) text)
+            options (or options {})]
+        (send-text* token chat-id options text))
+      (common/propagate by-path state event children)))
 
-(defn send-photo
-  [[key-token key-chat-id image options] & children]
-  (fn [by-path state event]
-    (let [token (key-token event)
-          chat-id (key-chat-id event)
-          image (if (or (fn? image) (keyword? image)) (image event) image)
-          options (or options {})]
-      (send-file* token chat-id options image "/sendPhoto" "photo" "photo.png"))
-    (common/propagate by-path state event children)))
+  (defn send-photo
+    [[key-token key-chat-id image options] & children]
+    (fn [by-path state event]
+      (let [token (key-token event)
+            chat-id (key-chat-id event)
+            image (if (or (fn? image) (keyword? image)) (image event) image)
+            options (or options {})]
+        (send-file* token chat-id options image "/sendPhoto" "photo" "photo.png"))
+      (common/propagate by-path state event children)))
 
-(defn new-offset
-  "Returns new offset for Telegram updates"
-  [result default]
-  (if (and result (< 0 (count result)))
+  (defn new-offset
+    "Returns new offset for Telegram updates"
+    [result default]
+    (if (and result (< 0 (count result)))
       (-> result last :update_id inc)
       default))
 
-(defn poller-error [e url params]
-  (try
-    (log/error (-> e
-                   .getData
-                   :body
-                   bs/to-string
-                   (json/read-str :key-fn keyword)
-                   (assoc :url url :params params)))
-    (catch Throwable t
-      {:error (.getMessage e) :url url :params params})))
+  (defn poller-error [e url params]
+    (try
+      (log/error (-> e
+                     .getData
+                     :body
+                     (json/read-str :key-fn keyword)
+                     (assoc :url url :params params)))
+      (catch Throwable t
+        {:error (.getMessage e) :url url :params params})))
 
-(defn poller [url params]
-  (try
-    (log/debug {:pooling url :params params})
-    (-> @(http/request {:request-method "get" :url url :query-params params})
-        :body
-        bs/to-string
-        (json/read-str :key-fn keyword))
-    (catch Exception e
-      (poller-error e url params))))
+  (defn poller [url params]
+    (try
+      (log/debug {:pooling url :params params})
+      (-> (http/get url {:query-params params})
+          :body
+          (json/read-str :key-fn keyword))
+      (catch Exception e
+        (poller-error e url params))))
 
-(defn start-server [token message-parser sink]
-  (log/info "Starting Telegram Bot Server, token: " token)
-  (let [url (str base-url token "/getUpdates")]
-    (go-loop [offset 0 limit 100] 
-      (let [params {:timeout 1 :offset offset :limit limit}
-            {:keys [ok result] :as data} (poller url params)]
-        (if ok
-          (dorun (map (fn [message]
-                        (let [parsed (and message-parser (message-parser (get-in message [:message :text])))]
-                          (sink (merge message parsed {:telegram/token token :telegram/chat-id (get-in message [:message :chat :id])}))))
-                      result))
-          (log/error data))
-        (<! (timeout 1000))
-        (recur (new-offset result offset) limit)))))
+  (defn start-server [token message-parser sink]
+    (log/info "Starting Telegram Bot Server, token: " token)
+    (let [url (str base-url token "/getUpdates")]
+      (go-loop [offset 0 limit 100] 
+        (let [params {:timeout 1 :offset offset :limit limit}
+              {:keys [ok result] :as data} (poller url params)]
+          (if ok
+            (dorun (map (fn [input]
+                          (let [chat-id (get-in input [:message :chat :id] (get-in input [:callback_query :message :chat :id]))
+                                parsed (and message-parser (message-parser (get-in input [:message :text])))]
+                            (sink (merge input parsed {:telegram/token token :telegram/chat-id chat-id}))))
+                        result))
+            (log/error data))
+          (<! (timeout 1000))
+          (recur (new-offset result offset) limit)))))
 
-(defmethod start-listener 'mx.interware.caudal.io.telegram
-  [sink config]
-  (let [{:keys [token parser]} (get-in config [:parameters])
-        parser-fn (if parser (if (symbol? parser) (resolve&get-fn parser) parser))]
-    (start-server token parser-fn sink)))
+  (defmethod start-listener 'mx.interware.caudal.io.telegram
+    [sink config]
+    (let [{:keys [token parser]} (get-in config [:parameters])
+          parser-fn (if parser (if (symbol? parser) (resolve&get-fn parser) parser))]
+      (start-server token parser-fn sink))))
