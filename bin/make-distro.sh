965fa68c359740a42615a6f0fdfd5887e9053f6e
Daniel Estevez <daniel.ef@gmail.com>
2019-06-17 22:09:26 -0500

    Fix broken command

--- bin/make-distro.sh
+++ bin/make-distro.sh
@@ -6,7 +6,7 @@
 export DKR_PATH="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && \
 export ROOT_PATH=$(dirname $DKR_PATH) && \
 export PROJECT=$( grep "defproject" $ROOT_PATH/project.clj ) && \
-export SERVICE_NAME=$( echo $PROJECT | awk '{split($2,t0,"/"); print(t0[2])}' ) && \
+export SERVICE_NAME=$( echo $PROJECT | awk '{print($2)}' ) && \
 export SERVICE_VERSION=$( echo $PROJECT | awk '{print(substr($3,2,length($3)-2))}' ) && \
 export DISTRO_DIR=$SERVICE_NAME-$SERVICE_VERSION && \
 cd $ROOT_PATH && \
