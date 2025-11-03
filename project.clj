72d907b42a188c79852e8cee54ea846036912679
Felipe de Jesus Gerard <felipe.gerard.c@gmail.com>
2025-11-03 10:14:45 -0600

    caudal 0.8.8

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   ;   the terms of this license.
   ;   You must not remove this notice, or any other, from this software
 
-  (defproject caudal "0.8.7"
-    :description "Caudal Platform 0.8.4"
+  (defproject caudal "0.8.8"
+    :description "Caudal Platform 0.8.8"
     :url "http://caudal.io/"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
